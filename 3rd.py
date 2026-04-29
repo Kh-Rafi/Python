ed9bacc99a770959d5978a1063276bb608a7675f
RAFI <mehedihasanrafi82@gmail.com>
2026-04-29 23:24:54 +0600

    3rd Commit

---
+++ 3rd.py
@@ -0,0 +1,7 @@
+age = int(input("Enter Number of Age: "))
+salary = int(input("Enter Salary: "))
+
+if age >= 30 and salary >= 40000:
+    print("Premium Member.")
+else:
+    print("Not Premium Member.")
