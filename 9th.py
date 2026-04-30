a212bca8dfaec3522035b098052d34b185a1ee7f
RAFI <mehedihasanrafi82@gmail.com>
2026-04-30 21:18:05 +0600

    9thCommit

---
+++ 9th.py
@@ -0,0 +1,16 @@
+num1 = 5
+print(num1, 'is of type', type(num1))
+
+num2 = 5.42
+print(num2, 'is of type', type(num2))
+
+num3 = 8+2j
+print(num3, 'is of type', type(num3))
+
+# Binary,Octal,Hexa
+
+print(0b1101011)  # prints 107
+
+print(0xFB + 0b10)  # prints 253
+
+print(0o15)  # prints 13
