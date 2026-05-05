42db681ad99aab9bb5cefebdce52d2a81fa0600d
RAFI <mehedihasanrafi82@gmail.com>
2026-05-05 21:04:55 +0600

    25th Commit

---
+++ 25th.py
@@ -0,0 +1,10 @@
+class Employee:
+    employee_id= 0
+
+employee1=Employee()
+employee2=Employee()
+
+employee1.employeeid=101
+print(f"Employee 1 Id :{employee1.employeeid}")
+employee2.employeeid=102
+print(f"Employee 2 Id :{employee2.employeeid}")
